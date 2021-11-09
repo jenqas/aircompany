72053226e8bf03dd741818d86e031bce35105dfd
Yauheni Sidarovich <yauheni_sidarovich@epam.com>
2021-11-09 16:31:36 +0300

    AirCompany - first commit

---
+++ Java/src/main/java/planes/OwnedPlane.java
@@ -0,0 +1,7 @@
+package planes;
+
+public class OwnedPlane  extends Plane {
+    public OwnedPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    }
+}
